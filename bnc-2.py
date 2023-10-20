66baabaa6c2bfe67cf79d5b4a4bc0bf20e4fe6b6
nessab35 <nessa.begay@students.dominican.edu>
2023-10-19 17:21:59 -0700

    updated bnc

--- bnc-2.py
+++ bnc-2.py
@@ -3,12 +3,13 @@ from random import randint
 
 
 def open_message():
+    '''Opening message'''
     print('''
           Get ready to play Bear, Ninja, Cowboy!
-          ''')
-    question_ = input("Would you like instructions? (yes/no)")
+          ''') #telling player to get ready to play
+    question_ = input("Would you like instructions? (yes/no)") # player is asked if they want instructions
 
-    if question_ == "yes":
+    if question_ == "yes": # ifn player says 'yes' then instructions are printed
         print('''
               Bear, Ninja, Cowboy is an exciting game of strategy and skill! 
               Pit your wit against the computer! 
@@ -20,66 +21,70 @@ def open_message():
               ''')
 
 
-def compare_choices(player, computer, player_score, total):
-    if computer == player:
+def compare_choices(player, computer, player_score, rounds_):
+    '''Compare player and computer'''
+    if computer == player: # if player = computer there's a draw
         print("DRAW!")
     elif computer == "Cowboy":
         if player == "Bear":
             print("You lose!", player, "is shot by", computer)
-            player_score -= 1
-        else: # computer is cowboy, player is ninja
+            player_score -= 1 # if player loses, a point is taken away
+        else: 
             print("You win!", player, "defeats", computer)
-            player_score += 1
+            player_score += 1 # if player wins, a point is gained
     elif computer == "Bear":
         if player == "Cowboy":
             print("You win!", player, "shoots", computer)
             player_score += 1
-        else: # computer is bear, player is ninja
+        else: 
             print("You lose!", player, "is eaten by", computer)
             player_score -= 1
     elif computer == "Ninja":
         if player == "Cowboy":
             print("You lose!", player, "is defeated by", computer)
             player_score -= 1
-        else: # computer is ninja, player is bear
+        else: 
             print("You win!", player, "eats", computer)
             player_score += 1
-    total += 1
-    return player_score, total
+    rounds_ += 1 # after each roud the total is counted for rounds
+    return player_score, rounds_ # return player_score and rounds_
 
 
-def play_again(player_score, total):
-    response = input("Would you like to play again? (yes/no) > ")
+def play_again(player_score, rounds_):
+    '''Play again'''
+    response = input("Would you like to play again? (yes/no) > ") # asking if player wants to play again
 
-    if response == "no":
-        print(f"Your final score: {player_score}/{total}")
-        return False
-    elif response == "yes":
-        return True
+    if response == "no": # if play again is no, score is printed out with how many rounds played
+        print(f"Your final score: {player_score}/{rounds_}")
+        return False # return false ends game
+    elif response == "yes": # if answer is yes
+        return True # game continues
     else:
-        print("Invalid input. Please enter 'yes' or 'no'.")
-        return play_again(player_score, total)  # Recursive call in case of invalid input
+        print("Invalid input. Please enter 'yes' or 'no'.") # if any other response is enter, error message pops up
+        return play_again(player_score, rounds_) # return funtion
 
 
 
-roles = ["Bear", "Ninja", "Cowboy"]
-player_score = 0
-total = 0
-play_game = True
-first_run = True
+roles = ["Bear", "Ninja", "Cowboy"] # assiging roles
+player_score = 0 # assigning player score to 0
+rounds_ = 0 # assigning rounds to 0
+play_game = True # play_again assigned to true
+first_run = True # first run assigned to true
 
+# as long as play_again is true, game runs
 while play_game:
-    if first_run:
-        open_message()
-        first_run = False
+    if first_run: # if it's first time playing
+        open_message() # open message is showed
+        first_run = False # then first run is returned false
 
-    computer = roles[randint(0,2)]
-    player_choice = input("Bear, Ninja, Cowboy?> ")
+    computer = roles[randint(0,2)] # assigned computer to random roles
+    player_choice = input("Bear, Ninja, Cowboy?> ") # player is asked to choose role
 
-    while player_choice not in roles:
+    while player_choice not in roles: # if player not in roles, error message occurs
         print("Invalid choice. Please choose from Bear, Ninja, or Cowboy.")
         player_choice = input("Bear, Ninja, Cowboy?> ")
 
-    player_score, total = compare_choices(player_choice, computer, player_score, total)
-    if not play_again(player_score, total):
+    # player_score, rounds_ is based off of the compare choices function
+    player_score, rounds_ = compare_choices(player_choice, computer, player_score, rounds_)
+    if not play_again(player_score, rounds_): # if player does not want play again, game ends
         break
